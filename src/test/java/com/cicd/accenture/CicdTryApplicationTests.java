90a5f1150f0d72113b27b8a276bdf26b4049403d
Glenmar <glenmar.c.santos@accenture.com>
2020-01-29 17:35:16 +0800

    New

---
+++ src/test/java/com/cicd/accenture/CicdTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.accenture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
